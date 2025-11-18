42c57c8fac15c230dc69cab9c2864d981d7435b7
masaakifukui <masaaki@lotusandcompanyinc.com>
2025-11-18 20:23:54 +0900

    test

--- api/recommendTest.js
+++ api/recommendTest.js
@@ -1,10 +1,11 @@
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 
-export default async function handler(req, res) {
+export default async function handler(event) {
     // GETの場合の 'prompt' を取得
-    var userPrompt = req.query.prompt;
-    console.log(userPrompt)
+    const itemId = event.queryStringParameters.id;
+    const item = await db.getItem(itemId);
+    console.log(item)
 //   try {
 //     // 2. 新しいプロンプトを構築（接頭辞とユーザーの入力を結合）
 //     //const combinedPrompt = "### 実行手順と制約1.  **入力データ**：提供されるニュースデータのtitle、内容、URLを読み取ります。2.  **要約の目的**：記事の最も重要な要点を抽出することに集中してください。3.  **制約**：* 各記事の要約は、**厳密に最大120文字以内**としなさい。* 出力は、以下の**JSON形式に厳密に従い**、他のコメントや説明は一切含めないこと。### 出力フォーマット（厳守）[{'title': '記事タイトル','summary': '要約文','url': '記事URL'},...]### ニュースデータ[title: 「世界ウミガメの日」｜研究・教育 - 名古屋港水族館,内容: また、卵の時の温度で性別が分かれる温度依存型性決定のため、地球<b>温暖化</b>の影響でメスばかりになってしまう可能性があると言われています。 身近なことで&nbsp;...,URL: https://nagoyaaqua.jp/study/column/26552/]"
